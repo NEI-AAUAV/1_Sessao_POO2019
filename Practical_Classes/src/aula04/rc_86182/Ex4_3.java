43fd254b1bb93ae1c9467b1426e998c1f0a11f52
user-cube <ruicoelho@ua.pt>
2019-03-23 14:55:05 +0000

    Ex 3

--- Practical_Classes/src/aula04/rc_86182/Ex4_3.java
+++ Practical_Classes/src/aula04/rc_86182/Ex4_3.java
@@ -5,8 +5,10 @@
  */
 package aula04.rc_86182;
 
+import java.util.Scanner;
+
 /**
- *
+ * Exercício 4.3.
  * @author rc
  */
 public class Ex4_3 {
@@ -15,7 +17,44 @@ public class Ex4_3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        /**
+         * Scanner que nos vai permitir ler a frase inserida pelo utilizador.
+         */
+        Scanner sc = new Scanner(System.in);
+        
+        /**
+         * Declaração de variáveis:
+         *  frase    - Frase inserida pelo utilizador.
+         *  acronimo - Acrónimo pedido.
+         */
+        String frase, acronimo = "";
+        
+        /**
+         * Declaração de variáveis:
+         *  words - Palavras que vão ser usadas para extrair as siglas.
+         */
+        String [] words;
+        
+        System.out.print("Frase: ");
+        frase = sc.nextLine(); //Lê a frase inserida pelo utilizador
+        
+        frase = frase.replaceAll("\\s+", " ");
+        words = frase.split(" "); //Faz split das palavras pelo espaço e guarda no array.
+        
+        for(String word:words) {
+            /**
+             * Verifica se a palavra tem um comprimento maior ou igual a 3
+             * e se começa por letra maiúscula, caso isso se verifique
+             * temos que a primeira letra dessa palavra fará parte do acrónimo.
+             */
+            if(word.length()>=3 && Character.isUpperCase(word.charAt(0))) {
+                acronimo += word.charAt(0); //Concatena as letras do acrónimo
+            }
+        }
+        
+        System.out.println(acronimo);
+		
     }
-    
 }
+    
